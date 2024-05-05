6626344c064744454b2642606a9e724138fb81ef
Priya Rani <priyarani0227@gmail.com>
2024-05-06 01:42:24 +0530

    filter  selection  component created and api data fetch

--- job-application/src/components/FetchCard.jsx
+++ job-application/src/components/FetchCard.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState,useEffect } from 'react';
 import JobCard from './JobCard';
 import '../styles/FetchCard.css';
 import JobFilter from './JobFilter';
@@ -6,6 +6,27 @@ import JobFilter from './JobFilter';
 
 function FetchCard() {
     const yourArray=[0,1,2,3,4,5,6,7,8,9]
+    const [data, setData] = useState(null);
+    
+  React.useEffect(()=>{
+    const myHeaders= new Headers();
+    myHeaders.append("Content-Type","application/json");
+    const body=JSON.stringify({
+      "limit":10,
+      "offset":0
+    });
+    const requestOptions={
+      method:"POST",
+      headers:myHeaders,
+      body
+    };
+    fetch("https://api.weekday.technology/adhoc/getSampleJdJSON",requestOptions)
+    .then(response => response.json())
+    // .then(data => setData(data,"---------------------->>>>data"))
+    .then((result)=>console.log(result,"-----------------------------result"))
+    .catch((error)=>console.log(error))
+    
+  })
     const fetchMoreData = () => {
         // Fetch the data and update the state
       };
--- job-application/src/components/JobFilter.jsx
+++ job-application/src/components/JobFilter.jsx
@@ -1,7 +1,45 @@
-import React from 'react';
+import React , {useState} from 'react';
 import '../styles/JobFilter.css';
+import Select from "react-select";
+const data=[
+    { value: 'chocolate', label: 'Chocolate' },
+    { value: 'strawberry', label: 'Strawberry' },
+    { value: 'vanilla', label: 'Vanilla' },
+]
 
 function JobFilter() {
+    const [filtered, setFiltered] = useState([]);
+    const [firstName, setFirstName] = useState("");
+    const onFilteredChangeCustom = (value, accessor) => {
+        console.log("typeof: " + typeof accessor);
+        console.log("value: " + value);
+    
+        let insertNewFilter = 1;
+        console.log(insertNewFilter);
+    
+        if (filtered.length) {
+          filtered.forEach((filter, i) => {
+            if (filter["id"] === accessor) {
+              if (value === "" || !value.length) {
+                filtered.splice(i, 1);
+              } else {
+                filter["value"] = value;
+              }
+              insertNewFilter = 0;
+            }
+          });
+        }
+    
+        if (insertNewFilter) {
+          var customFilter = [];
+          customFilter.push({ id: accessor, value: value });
+          console.log("CUSTOM FILTER:" + customFilter);
+        }
+    
+        setFiltered({ customFilter: filtered });
+        console.log("setFiltered: " + filtered);
+      };
+  
   return (
     <div className='jobFilter'>
       <div className='jobVarient'>
@@ -9,6 +47,24 @@ function JobFilter() {
         <div className='searchJob'>Search jobs</div>
         <div>Suggested jobs</div>
       </div>
+      <Select
+        onChange={(entry) => {
+          setFirstName({ firstName: entry });
+          console.log("entered:" + firstName);
+          onFilteredChangeCustom(
+            entry.map((o) => {
+              return o.value;
+            }),
+            "firstName"
+          );
+        }}
+        value={filtered.firstName}
+        isMulti
+        closeMenuOnSelect={false}
+        options={data.map((o, i) => {
+          return { id: i, value: o.firstName, label: o.firstName };
+        })}
+      />
     </div>
   )
 }
