5b59d80000a44529dc95aae8178d89e353b8575c
Priya Rani <priyarani0227@gmail.com>
2024-05-06 14:40:38 +0530

    infinite scrolling

--- job-application/src/components/FetchCard.jsx
+++ job-application/src/components/FetchCard.jsx
@@ -1,39 +1,21 @@
-import React, { useState,useEffect } from 'react';
-import JobCard from './JobCard';
-import '../styles/FetchCard.css';
-import JobFilter from './JobFilter';
-
+import React, { useState, useEffect } from "react";
+import JobCard from "./JobCard";
+import "../styles/FetchCard.css";
+import JobFilter from "./JobFilter";
 
 function FetchCard() {
-    // const yourArray=[0,1,2,3,4,5,6,7,8,9]
-    
- 
-    const fetchMoreData = () => {
-        // Fetch the data and update the state
-      };
-    React.useEffect(() => {
-        window.addEventListener('scroll', handleScroll);
-        return () => window.removeEventListener('scroll', handleScroll);
-      }, []);
-    
-      // Function to handle the scroll event
-      const handleScroll = () => {
-        if (window.innerHeight + window.scrollY >= document.body.offsetHeight) {
-           fetchMoreData();
-        }
-      };
-    
   return (
-    <div className='container-fluid'>
-        <div><JobFilter/></div>
-        <div className='row'>
-          
-   <div className='colStyle'><JobCard/></div> 
-
-     
+    <div className="container-fluid">
+      <div>
+        <JobFilter />
+      </div>
+      <div className="row">
+        <div className="colStyle">
+          <JobCard />
+        </div>
       </div>
     </div>
-  )
+  );
 }
 
 export default FetchCard;
--- job-application/src/components/JobCard.jsx
+++ job-application/src/components/JobCard.jsx
@@ -1,32 +1,34 @@
-import React from 'react';
-import Box from '@mui/material/Box';
-import Card from '@mui/material/Card';
-import CardActions from '@mui/material/CardActions';
-import CardContent from '@mui/material/CardContent';
-import Button from '@mui/material/Button';
-import Typography from '@mui/material/Typography';
-import '../../src/styles/JobCard.css';
-import Logo from '../assets/images/GoogleLogo.png';
+import React from "react";
+import Card from "@mui/material/Card";
+import CardActions from "@mui/material/CardActions";
+import CardContent from "@mui/material/CardContent";
+import Typography from "@mui/material/Typography";
+import "../../src/styles/JobCard.css";
 import UserPic from "../assets/images/UserPic.jpeg";
 
 function JobCard() {
   const [data, setData] = React.useState([""]);
-
-  React.useEffect(()=>{
-    const myHeaders= new Headers();
-    myHeaders.append("Content-Type","application/json");
-    const body=JSON.stringify({
-      "limit":10,
-      "offset":0
+  const [page, setPage] = React.useState(1);
+  const [fetching, setFetching] = React.useState(false);
+  React.useEffect(() => {
+    const myHeaders = new Headers();
+    myHeaders.append("Content-Type", "application/json");
+    const body = JSON.stringify({
+      limit: 10,
+      offset: 0,
     });
-    const requestOptions={
-      method:"POST",
-      headers:myHeaders,
-      body
+    const requestOptions = {
+      method: "POST",
+      headers: myHeaders,
+      body,
     };
     const fetchData = async () => {
       try {
-        const response = await fetch("https://api.weekday.technology/adhoc/getSampleJdJSON", requestOptions);
+        setFetching(true);
+        const response = await fetch(
+          "https://api.weekday.technology/adhoc/getSampleJdJSON",
+          requestOptions
+        );
         const result = await response.json();
         // for (let i = 0; i < result.jdList.length; i++) {
         // console.log("Fetched data:", result.jdList[i]);
@@ -36,70 +38,95 @@ function JobCard() {
         // setData(PriyaData);
         // }
         console.log(result.jdList);
-        setData(result.jdList);
+        // setData(result.jdList);
+        setData((prevData) => [...prevData, ...result.jdList]);
+        setPage((prevPage) => prevPage + 1);
+        setFetching(false);
       } catch (error) {
-        console.error('Error fetching data:', error);
+        console.error("Error fetching data:", error);
+        setFetching(false);
       }
     };
 
     fetchData();
-  },[]);
+  }, [page]);
+
+  React.useEffect(() => {
+    const handleScroll = () => {
+      if (window.innerHeight + window.scrollY >= document.body.scrollHeight) {
+        if (!fetching) {
+          setPage((prevPage) => prevPage + 1);
+        }
+      }
+    };
+
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, [fetching]);
   return (
     <>
-    {data && (
-    <div className='jobCard'>
-      {data.map((item, index)=>(
-       <Card sx={{ minWidth: 275 }} className='cardContainer' key={index}>
-      <CardContent>
-        <div className='postedDate' gutterBottom>
-          <p>⏳ Posted on 17 days ago </p>
-        </div>
-        
-        <div >
-            <div className='wrapinf'>
-            <div className='imgStyle'>
-            <img src={item.logoUrl} style={{width:"100%"}}/>
-             </div>
-             
-            <div className='infoContainer'>
-            <div className="MuiBox-root">{item.companyName}</div>
-            <div className='JobName'>{item.jobRole}</div>
-            <div className="cards-sub-text">{item.location}</div>
-            </div>
-            
-            </div>
-           </div>
-       
-        <Typography variant="body2" className='salaryClass'>
-        Estimated Salary: ₹15 - 40 LPA ✅
-        </Typography>
-        <Typography className='companyWrap'>
-            <div className='aboutCompany'>About Company:</div>
-            <div className='aboutUs'>About us</div>
-            <div className='description'>{item.jobDetailsFromCompany}</div>
-                 <div>Founder/Recruiter profiles:</div>
-                 <div className='founderName'><a href="https://www.linkedin.com/in/dubeysuchit"><span style= {{fontWeight:400}}>Suchit Dubey</span></a></div>
-        </Typography>
-        <div className='viewWholeJob'><a href="https://pp.com">View job</a></div>
-        <div className='exp-info-container'>
-            <div>Minimum Experience</div>
-            <div>{item.minExp} years</div>
-        </div>
-      </CardContent>
-      <CardActions className='cardActionbtn'>
-        <button  className="buttonApply">⚡ Easy Apply</button>
-        <button  className="buttonRefrral">
-            <div style={{width:"10%"}}><img src={UserPic} className='userPicStyle'/></div>
-            <div className='userRefer'>Ask for referral</div>
-        </button>
+      {data && (
+        <div className="jobCard">
+          {data.map((item, index) => (
+            <Card sx={{ minWidth: 275 }} className="cardContainer" key={index}>
+              <CardContent>
+                <div className="postedDate" gutterBottom>
+                  <p>⏳ Posted on 17 days ago </p>
+                </div>
 
-      </CardActions>
-    </Card>
-    ))}
-    </div>
-    )}
+                <div>
+                  <div className="wrapinf">
+                    <div className="imgStyle">
+                      <img src={item.logoUrl} style={{ width: "100%" }} />
+                    </div>
+
+                    <div className="infoContainer">
+                      <div className="MuiBox-root">{item.companyName}</div>
+                      <div className="JobName">{item.jobRole}</div>
+                      <div className="cards-sub-text">{item.location}</div>
+                    </div>
+                  </div>
+                </div>
+
+                <Typography variant="body2" className="salaryClass">
+                  Estimated Salary: ₹15 - 40 LPA ✅
+                </Typography>
+                <Typography className="companyWrap">
+                  <div className="aboutCompany">About Company:</div>
+                  <div className="aboutUs">About us</div>
+                  <div className="description">
+                    {item.jobDetailsFromCompany}
+                  </div>
+                  <div>Founder/Recruiter profiles:</div>
+                  <div className="founderName">
+                    <a href="https://www.linkedin.com/in/dubeysuchit">
+                      <span style={{ fontWeight: 400 }}>Suchit Dubey</span>
+                    </a>
+                  </div>
+                </Typography>
+                <div className="viewWholeJob">
+                  <a href="https://pp.com">View job</a>
+                </div>
+                <div className="exp-info-container">
+                  <div>Minimum Experience</div>
+                  <div>{item.minExp} years</div>
+                </div>
+              </CardContent>
+              <CardActions className="cardActionbtn">
+                <button className="buttonApply">⚡ Easy Apply</button>
+                <button className="buttonRefrral">
+                  <div style={{ width: "10%" }}>
+                    <img src={UserPic} className="userPicStyle" />
+                  </div>
+                  <div className="userRefer">Ask for referral</div>
+                </button>
+              </CardActions>
+            </Card>
+          ))}
+        </div>
+      )}
     </>
-  )
+  );
 }
 
-export default JobCard
+export default JobCard;
